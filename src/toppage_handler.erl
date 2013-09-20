ad0fb006a58e50ec9b345dac159dab941b347986
Pierre Fenoll <pierrefenoll@gmail.com>
2013-09-20 14:12:25 +0100

    Fixed path resolution of priv/index.htm

--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -11,17 +11,19 @@
 %% API
 
 init(_Transport, Req, []) ->
-	{ok, Req, undefined}.
+    {ok, Req, undefined}.
 
 handle(Req, State) ->
-	{ok, Body} = file:read_file("priv/index.htm"), %% File IO is not an issue.
-	{ok, Req2} = cowboy_req:reply(200,
-								  [{<<"content-type">>, <<"text/html">>}],
-								  Body, Req),
-	{ok, Req2, State}.
+    S = code:which(icy),
+    Dir = string:sub_string(S, 1, length(S) - length("ebin.icy.beam")),
+    {ok, Body} = file:read_file(Dir ++ "priv/index.htm"), %% File IO is not an issue.
+    {ok, Req2} = cowboy_req:reply(200,
+                                  [{<<"content-type">>, <<"text/html">>}],
+                                  Body, Req),
+    {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
-	ok.
+    ok.
 
 %% Internals
 
