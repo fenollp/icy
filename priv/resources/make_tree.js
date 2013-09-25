5be5e89eb09840865dae738ce2201dc3f9a4d7e9
Pierre Fenoll <pierrefenoll@gmail.com>
2013-09-25 11:02:16 +0100

    refactoring

--- priv/resources/make_tree.js
+++ priv/resources/make_tree.js
@@ -8,51 +8,6 @@ function renderText2(nodes, edges){
   renderJSObjsToD3(nodes, edges, ".main-svg");
 };
 
-function renderText(dataParsed) {
-   var nodes = {};
-   var edges = [];
-
-   dataParsed.forEach(function (e) {
-     populate(e, nodes, edges);
-   });
-
-  console.log(JSON.stringify({"nodes":nodes}));////////////////////////////////////////////////////////////////
-  console.log(JSON.stringify({"edges":edges}));////////////////////////////////////////////////////////////////
-   renderJSObjsToD3(nodes, edges, ".main-svg");
-};
-
-function populate(data, nodes, edges) {
-  var nodeID = Object.keys(nodes).length;
-
-  var newNode = {
-	  label: (data.data.word) ? data.data.word : data.data.type,
-    id: nodeID + ""
-  };
-
-  var classes = ["type-" + data.data.type];
-  if (data.data.ne) {
-    classes.push("ne-" + data.data.ne);
-  }
-
-  newNode.nodeclass = classes.join(" ");
-
-  //  I hate javascript
-  nodes[nodeID] = newNode;
-
-  data.children.forEach(function (child) {
-    var newChild = populate(child, nodes, edges);
-
-    edges.push({
-      source: newNode.id,
-      target: newChild.id,
-      id: newNode.id + "-" + newChild.id
-    });
-  });
-
-  return newNode;
-}
-
-
 function buildGraphData(node, nodes, links) {
   var index = nodes.length;
   nodes.push({
